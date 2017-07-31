cdc71375b3eba2609a47884529a77198b03844e9
Bailey <bailey.g.weathers@gmail.com>
2017-07-31 05:39:01 -0500

    Complete  03-checkpoint-Architecture-and-Design-Patterns-ORM

--- lib/bloc_works.rb
+++ lib/bloc_works.rb
@@ -7,7 +7,7 @@ require "bloc_works/controller"
 module BlocWorks
   class Application
     def call(env)
-      [200, {'Content-Type' => 'text/html'}, ["Hello Blocheads!"]]
+      [200, {'Content-Type' => 'text/html'}, ["Hello All Blocheads!"]]
     end
   end
 end
